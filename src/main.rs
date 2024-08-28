4fece35cbe94c136499a1e62e401199c9baa9484
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-08-28 11:07:21 +0200

    feat: render HTML from markdown in main fn.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use leptos::*;
+use pulldown_cmark::{Parser, Options};
+use ammonia::Builder;
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+
+    let markdown_input = "Hello world, this is a *very simple* example.";
+    let options = Options::empty();
+    let parser = Parser::new_ext(markdown_input, options);
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+
+    let sanitized_html = Builder::default().clean(&html_output).to_string();
+    
+    mount_to_body(move || view! { 
+        <div inner_html=sanitized_html />
+    });
 }
