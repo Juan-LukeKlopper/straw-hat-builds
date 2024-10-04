fa7c2cc2890c1fa6b26819ca4cc81c0b9fd2fdd0
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-04 11:16:43 +0200

    chore: remove unused code

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -4,7 +4,6 @@ use leptos::*;
 pub fn About() -> impl IntoView {
     view! {
         <div class="w-full bg-gradient-to-r from-purple-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-8 ">
-                          //<footer class="bg-gradient-to-b from-slate-950 via-gray-800 to-slate-950">
 
         <div class="max-w-8xl mx-auto p-8 my-8 text-gray-100 border-2 border-black rounded ring ring-offset-2 ring-slate-900 bg-slate-900 text-lg text-justify">
             <h1 class="text-4xl font-bold mb-6 text-center">"About Straw-Hat Builds"</h1>
