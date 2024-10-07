fbcd241213b0aed3aec317a29ba9d478915e0de7
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 00:26:43 +0200

    chore: Remove header left from previous page

--- src/pages/projects.rs
+++ src/pages/projects.rs
@@ -5,11 +5,9 @@ use leptos::*;
 pub fn Project() -> impl IntoView {
     view! {
         <div class="w-full bg-gradient-to-r from-purple-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-8 text-center ">
-        <div class="max-w-8xl mx-auto p-8 my-8 text-gray-100 border-2 border-black rounded ring ring-offset-2 ring-slate-900 bg-slate-900 text-lg text-justify">
-        <h1>"Welcome to StrawHatBuilds!"</h1>
-        <p>"Gaze apon all of the treasure maps we have collected!"</p>
-        <Project_output/>
-        </div>
+            <div class="max-w-8xl mx-auto p-8 my-8 text-gray-100 border-2 border-black rounded ring ring-offset-2 ring-slate-900 bg-slate-900 text-lg text-justify">
+                <Project_output/>
+            </div>
         </div>
     }
 }
