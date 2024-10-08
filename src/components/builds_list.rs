a5022652430e10889670b3aa8ded460e2ee6ddac
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 10:45:45 +0200

    chore: rename component

--- src/components/builds_list.rs
+++ src/components/builds_list.rs
@@ -2,29 +2,29 @@ use leptos::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Project {
+pub struct Build {
     pub name: String,
     pub description: String,
 }
 
-#[server(GetAvailablePosts, "/api")]
-pub async fn get_available_posts() -> Result<Vec<Project>, ServerFnError> {
+#[server(GetAvailableBuilds, "/api")]
+pub async fn get_available_builds() -> Result<Vec<Build>, ServerFnError> {
     use std::fs;
     let content_dir = "./content"; // Adjust this to your actual content directory path
-    let mut available_posts = Vec::new();
+    let mut available_builds = Vec::new();
 
     if let Ok(entries) = fs::read_dir(content_dir) {
         for entry in entries {
             if let Ok(entry) = entry {
                 let file_name = entry.file_name().into_string().unwrap_or_default();
-                let project_dir = entry.path();
+                let build_dir = entry.path();
 
-                if project_dir.is_dir() {
+                if build_dir.is_dir() {
                     // Default description if README.md doesn't exist
                     let mut description = "No description available.".to_string();
 
                     // Check if README.md exists in the project folder
-                    let readme_path = project_dir.join("README.md");
+                    let readme_path = build_dir.join("README.md");
                     if readme_path.exists() {
                         // Read the contents of README.md
                         if let Ok(content) = fs::read_to_string(readme_path) {
@@ -33,7 +33,7 @@ pub async fn get_available_posts() -> Result<Vec<Project>, ServerFnError> {
                     }
 
                     // Push the project with name and description
-                    available_posts.push(Project {
+                    available_builds.push(Build {
                         name: file_name,
                         description,
                     });
@@ -42,29 +42,28 @@ pub async fn get_available_posts() -> Result<Vec<Project>, ServerFnError> {
         }
     }
 
-    println!("Available_projects = {:?}", available_posts);
-    Ok(available_posts)
+    Ok(available_builds)
 }
 
 #[component]
-pub fn Posts() -> impl IntoView {
-    let (posts, _set_posts) = create_signal(vec!["".to_string()]);
+pub fn builds_list() -> impl IntoView {
+    let (builds, _set_builds) = create_signal(vec!["".to_string()]);
 
-    let async_projects = create_resource(
+    let async_builds = create_resource(
         // the first is the "source signal"
-        posts,
+        builds,
         // the second is the loader
         // it takes the source signal's value as its argument
         // and does some async work
-        |_value| async move { get_available_posts().await.unwrap() },
+        |_value| async move { get_available_builds().await.unwrap() },
     );
 
-    let async_result = move || {
-        async_projects.get().map(|projects| {
+    let list_of_builds = move || {
+        async_builds.get().map(|builds| {
             view! {
                 <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12 space-y-6">
-                    {projects.iter().map(|project| {
-                        let background_image_url = format!("/backgrounds/{}.svg", project.name.replace(" ", "%20"));
+                    {builds.iter().map(|build| {
+                        let background_image_url = format!("/backgrounds/{}.svg", build.name.replace(" ", "%20"));
 
                         view! {
                             <div class="flex h-[400px] overflow-hidden rounded-lg bg-gray-100">
@@ -74,13 +73,13 @@ pub fn Posts() -> impl IntoView {
                                 ></div>
 
                                 <div class="w-1/2 p-12 flex flex-col justify-center text-center bg-black text-white">
-                                    <h2 class="mb-4 text-4xl font-semibold">{&project.name}</h2>
-                                    <h4 class="mb-6 text-xl font-semibold">{&project.description}</h4>
+                                    <h2 class="mb-4 text-4xl font-semibold">{&build.name}</h2>
+                                    <h4 class="mb-6 text-xl font-semibold">{&build.description}</h4>
                                     <button
                                       type="button"
                                       class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-300 hover:text-neutral-200 focus:border-neutral-300 focus:text-neutral-200 focus:outline-none focus:ring-0 active:border-neutral-300 active:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600"
                                     >
-                                    <a href={format!{"/builds/{}/{}/{}",&project.name, 0, 1}}>
+                                    <a href={format!{"/builds/{}/{}/{}",&build.name, 0, 1}}>
                                       Learn More
                                     </a>
                                     </button>
@@ -93,7 +92,7 @@ pub fn Posts() -> impl IntoView {
         })
     };
 
-    let loading = async_projects.loading();
+    let loading = async_builds.loading();
     let _is_loading = move || if loading() { "Loading..." } else { "Idle." };
 
     view! {
@@ -101,7 +100,7 @@ pub fn Posts() -> impl IntoView {
             <Suspense
                 fallback=move || view! { <p>"Loading..."</p> }
             >
-                {move || async_result().unwrap_or_else(|| view! { <div><li>"Loading..."</li></div> })}
+                {move || list_of_builds().unwrap_or_else(|| view! { <div><li>"Loading..."</li></div> })}
             </Suspense>
         </div>
     }
