cb70682d1e048cd0cb52e2c2b5ad55946a488a37
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-08 10:47:26 +0200

    feat: add builds page and declare it

--- src/pages/builds.rs
+++ src/pages/builds.rs
@@ -1,13 +1,16 @@
+use crate::components::builds_list::*;
 use leptos::*;
 
 /// Renders the home page of your application.
 #[component]
-pub fn Home() -> impl IntoView {
+pub fn builds() -> impl IntoView {
     view! {
         <div class="w-full bg-gradient-to-r from-purple-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-8 text-center ">
         <div class="max-w-8xl mx-auto p-8 my-8 text-gray-100 border-2 border-black rounded ring ring-offset-2 ring-slate-900 bg-slate-900 text-lg text-justify">
-        <h1>"Welcome to StrawHatBuilds!"</h1>
+        <h1>"Welcome to our builds page!"</h1>
         <p>"Gaze apon all of the treasure maps we have collected!"</p>
+        <p>"Click Learn More to start your expedition"</p>
+        <BuildsList/>
         </div>
         </div>
     }
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,3 +1,4 @@
 pub mod about;
+pub mod builds;
 pub mod home;
 pub mod projects;
