f05417e33837d1197f26f7599eec4779bc083643
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-16 00:29:10 +0200

    chore: change typing to snake case

--- src/pages/projects.rs
+++ src/pages/projects.rs
@@ -2,11 +2,11 @@ use crate::components::project::*;
 use leptos::*;
 
 /// Renders the home page of your application.
-pub fn Project() -> impl IntoView {
+pub fn project() -> impl IntoView {
     view! {
         <div class="w-full bg-gradient-to-r from-purple-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-8 text-center ">
             <div class="max-w-8xl mx-auto p-8 my-8 text-gray-100 border-2 border-black rounded ring ring-offset-2 ring-slate-900 bg-slate-900 text-lg text-justify">
-                <Project_output/>
+                <ProjectOutput/>
             </div>
         </div>
     }
