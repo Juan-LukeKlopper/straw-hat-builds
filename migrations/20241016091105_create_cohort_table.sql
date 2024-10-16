415c728ac02ff11c7ba0aeb6c9a78736bf22a1f5
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-10-16 09:54:32 +0200

    feat: Change migration file to match new db reqs

---
+++ migrations/20241016091105_create_cohort_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE cohorts (
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(45) NOT NULL,
+    build VARCHAR(100) NOT NULL,
+    tx_hash VARCHAR(64), 
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
